2153a98c0c9f07d9fb1ad02cea8d2a90732afa7b
Sergiu Suciu <sergiu.suciu@lifeishard.ro>
2017-07-04 17:39:09 +0300

    initial commit

---
+++ WikiScraps/WikiScraps/spiders/judete_orase.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class JudeteOraseSpider(scrapy.Spider):
+    name = 'judete_orase'
+    allowed_domains = ['wikipedia.org']
+    start_urls = ['https://ro.wikipedia.org/wiki/Liste_de_localit%C4%83%C8%9Bi_din_Rom%C3%A2nia_grupate_pe_jude%C8%9Be']
+
+    def parse(self, response):
+        urls = response.css('p b a::attr(href)').extract()
+        for url in urls:
+            print url
+            url = response.urljoin(url)
+            print url
+            yield scrapy.Request(url=url, callback=self.parse_details)
+
+    
+    def parse_details(self, response):
+        yield {
+            'judet' : response.css('h1::text').extract_first() ,
+            'orase' : response.css('#mw-content-text > div > table > tr > td:nth-child(1) > a::text').extract()
+        }
